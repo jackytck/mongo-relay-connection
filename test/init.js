4d188bd53ac55b6b9e42929d76c091285546f32e
Jacky Tang <jackytck@gmail.com>
2017-04-17 13:28:19 +0800

    :white_check_mark: connect mongoose

---
+++ test/init.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+mongoose.Promise = global.Promise
+
+// executed only once
+before(done => {
+  mongoose.connect('mongodb://localhost/mongo-relay-connection-test')
+  mongoose.connection
+    .once('open', () => {
+      console.log('Connected to mongo')
+      done()
+    })
+    .on('error', error => {
+      console.warn('Warning', error)
+    })
+})
