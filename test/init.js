ee1e9fcc9b0075b8bc919caf9f8a4c9b67b41800
Jacky Tang <jackytck@gmail.com>
2017-04-19 01:01:11 +0800

    :sparkles: clear + populate sample data to mongo

--- test/init.js
+++ test/init.js
@@ -1,13 +1,30 @@
 import mongoose from 'mongoose'
+import Starship from './models/starship'
+import data from './starships.json'
 
 mongoose.Promise = global.Promise
+
+function clearDB () {
+  return Starship.remove()
+}
+
+function populateData () {
+  const { data: { allStarships: { edges } } } = data
+  return Promise.all(edges.map(({ node }) => {
+    const ship = new Starship(node)
+    return ship.save()
+  }))
+}
 
 // executed only once
 before(done => {
   mongoose.connect('mongodb://localhost/mongo-relay-connection-test')
   mongoose.connection
-    .once('open', () => {
+    .once('open', async () => {
       console.log('Connected to mongo')
+      await clearDB()
+      await populateData()
+      console.log('Populated data')
       done()
     })
     .on('error', error => {
