faedb47bb38bc11a74bc9b4538c8655ac977401e
Jacky Tang <jackytck@gmail.com>
2017-04-23 11:42:59 +0800

    :white_check_mark: add pageInfo test

--- test/index.js
+++ test/index.js
@@ -4,14 +4,14 @@ import { graphql } from 'graphql'
 import Starship from './models/starship'
 import schema from './schema/schema'
 
-describe('mongo test', () => {
+describe('mongo data', () => {
   it('should fetch correct number of starships from mongo', async () => {
     const cnt = await Starship.count()
     assert(cnt === 36)
   })
 })
 
-describe('query totalCount', () => {
+describe('totalCount', () => {
   it('should fetch correct total count', async () => {
     const query = `
       {
@@ -22,5 +22,29 @@ describe('query totalCount', () => {
     `
     const res = await graphql(schema, query)
     assert(res.data.allStarships.totalCount === 36)
+  })
+})
+
+describe('pageInfo', () => {
+  it('should fetch correct page info', async () => {
+    const query = `
+      {
+        allStarships {
+          pageInfo {
+            hasNextPage
+            hasPreviousPage
+            startCursor
+            endCursor
+          }
+        }
+      }
+    `
+    const res = await graphql(schema, query)
+    const { pageInfo } = res.data.allStarships
+    const { hasNextPage, hasPreviousPage, startCursor, endCursor } = pageInfo
+    assert(hasNextPage === false)
+    assert(hasPreviousPage === false)
+    assert(startCursor)
+    assert(endCursor)
   })
 })
