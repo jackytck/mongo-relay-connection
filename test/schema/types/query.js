a54d54aa67647e70161e1a92a919ff0f075b8c2a
Jacky Tang <jackytck@gmail.com>
2017-05-03 00:05:09 +0800

    :sparkles: allFoodProducts graphql query

--- test/schema/types/query.js
+++ test/schema/types/query.js
@@ -8,6 +8,8 @@ import {
 } from '../../../src'
 import Starship from './starship'
 import StarshipModel from '../../models/starship'
+import Product from './product'
+import ProductModel from '../../models/product'
 
 const RootQuery = new GraphQLObjectType({
   name: 'RootQuery',
@@ -20,6 +22,32 @@ const RootQuery = new GraphQLObjectType({
           cursorField: 'starshipClass'
         }
         return mrResolve(args, StarshipModel, {}, opts)
+      }
+    },
+    allFoodProducts: {
+      type: mrType('FoodProduct', Product),
+      args: mrArgs,
+      resolve (parentValue, args) {
+        const query = {
+          type: {
+            $in: [
+              'Bacon',
+              'Cheese',
+              'Chicken',
+              'Chips',
+              'Fish',
+              'Pizza',
+              'Salad',
+              'Sausages',
+              'Tuna'
+            ]
+          }
+        }
+        const opts = {
+          cursorField: 'price',
+          direction: -1
+        }
+        return mrResolve(args, ProductModel, query, opts)
       }
     }
   }
